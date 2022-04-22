ac4a4c2bd59023edcb4b5c43595852212103e4aa
pipe119 <felipe.piedrahita@hotmail.com>
2022-04-22 17:43:52 -0500

    Se agregaron los campos de apellido y telefono. Tambien se modificaron los iconos de los botones

---
+++ sistema/src/sistema/empleadosBL/empleadosBL.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistema.empleadosBL;
+
+/**
+ *Creamos los métodos setters y getters de cada elemento
+ * @author Daniel
+ */
+public class empleadosBL {
+    
+    int ID;
+    String nombre, apellido, correo, telefono;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    
+    public void setApellido(String nombre) {
+        this.apellido = apellido;
+    }
+    
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    public String getTelefono() {
+        return telefono;
+    }
+    
+    public void setTelefono(String correo) {
+        this.telefono = telefono;
+    }
+    
+}
